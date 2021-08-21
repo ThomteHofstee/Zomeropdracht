22685a86ef6621476643d5f3f6bcd31740b93549
M. te Hofstee <mtehofstee@MacBook-Air-van-M.local>
2021-08-21 13:49:05 +0200

    progres

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,7 +2,26 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 class ArticleController
 {
+    /**
+     * @Route("/")
+     */
+        public function homepage(): Response
+        {
+            return new Response('omg my first page already');
+        }
 
+        /**
+         * @Route("/news/{slug}")
+         */
+        public function show($slug)
+        {
+            return new Response(sprintf('Hier komt de opdracht van school %s',
+            $slug
+            ));
+        }
 }
