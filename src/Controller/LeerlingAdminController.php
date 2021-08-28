df7d5124d6c1f773c7b69786cc835c90d00ec8bd
M. te Hofstee <mtehofstee@MacBook-Air-van-M.local>
2021-08-28 20:52:00 +0200

    update tabel+database

--- src/Controller/LeerlingAdminController.php
+++ src/Controller/LeerlingAdminController.php
@@ -13,31 +13,128 @@ class LeerlingAdminController extends AbstractController
 {
 
     /**
-     * @Route("/admin/article/new")
+     * @Route("/admin/leerlingenlijst/new")
      */
     public function new(EntityManagerInterface $em)
     {
         $leerlingen = new Leerlingen();
-        $leerlingen->setVoornaam('Thom')
-            ->setTussenvoegsel('de-leerlingenlijst-van-2021/2022'.rand(100,999));
 
-
-        if(rand(1, 10) > 2)
-        {
-            $leerlingen->setAchternaam('Hofstee');
-        }
-
-
+        $leerlingen->setVoornaam('Joost');
+        $leerlingen->setAchternaam('Van den Berg');
         $em->persist($leerlingen);
+        $em->flush();
 
 
+        $leerlingen = new Leerlingen();
+        $leerlingen->setVoornaam('Youssef');
+        $leerlingen->setAchternaam('Ali');
+        $em->persist($leerlingen);
+        $em->flush();
+
+        $leerlingen = new Leerlingen();
+        $leerlingen->setVoornaam('Hugo');
+        $leerlingen->setAchternaam('Bouwmeester');
+        $em->persist($leerlingen);
+        $em->flush();
+
+
+        $leerlingen = new Leerlingen();
+        $leerlingen->setVoornaam('Roan');
+        $leerlingen->setAchternaam('Swagerman');
+        $em->persist($leerlingen);
+        $em->flush();
+
+
+        $leerlingen = new Leerlingen();
+        $leerlingen->setVoornaam('Mohammed');
+        $leerlingen->setAchternaam('Al-khadid');
+        $em->persist($leerlingen);
+        $em->flush();
+
+
+        $leerlingen = new Leerlingen();
+        $leerlingen->setVoornaam('Jayden');
+        $leerlingen->setAchternaam('Arabi');
+        $em->persist($leerlingen);
+        $em->flush();
+
+
+        $leerlingen = new Leerlingen();
+        $leerlingen->setVoornaam('Lucas');
+        $leerlingen->setAchternaam('Greve');
+        $em->persist($leerlingen);
+        $em->flush();
+
+        $leerlingen = new Leerlingen();
+        $leerlingen->setVoornaam('Thom');
+        $leerlingen->setAchternaam('Hofstee');
+        $leerlingen->setTussenvoegsel('te');
+        $em->persist($leerlingen);
+        $em->flush();
+
+
+        $leerlingen = new Leerlingen();
+        $leerlingen->setVoornaam('Beau');
+        $leerlingen->setAchternaam('Reijers');
+        $em->persist($leerlingen);
+        $em->flush();
+
+
+        $leerlingen = new Leerlingen();
+        $leerlingen->setVoornaam('Yimmie');
+        $leerlingen->setAchternaam('van dubbelden');
+        $em->persist($leerlingen);
+        $em->flush();
+
+
+        $leerlingen = new Leerlingen();
+        $leerlingen->setVoornaam('Kevin');
+        $leerlingen->setAchternaam('Vis');
+        $em->persist($leerlingen);
+        $em->flush();
+
+
+        $leerlingen = new Leerlingen();
+        $leerlingen->setVoornaam('Robbie');
+        $leerlingen->setAchternaam('Graaf');
+        $leerlingen->setTussenvoegsel('van de');
+        $em->persist($leerlingen);
+        $em->flush();
+
+
+        $leerlingen = new Leerlingen();
+        $leerlingen->setVoornaam('Dion');
+        $leerlingen->setAchternaam('Boer');
+        $em->persist($leerlingen);
+        $em->flush();
+
+        $leerlingen = new Leerlingen();
+        $leerlingen->setVoornaam('Sven');
+        $leerlingen->setAchternaam('Counette');
+        $em->persist($leerlingen);
+        $em->flush();
+
+        $leerlingen = new Leerlingen();
+        $leerlingen->setVoornaam('Ahmad');
+        $leerlingen->setAchternaam('Darwich');
+        $em->persist($leerlingen);
+        $em->flush();
+
+        $leerlingen = new Leerlingen();
+        $leerlingen->setVoornaam('Jorden');
+        $leerlingen->setAchternaam('Pultrum');
+        $em->persist($leerlingen);
+        $em->flush();
+
+        dump($leerlingen);die;
 
 
         return new Response(sprintf(
-            'Hiya! new article id: #$d voornaam: %s',
+            'Hiya! new article id: #%d voornaam: %s',
             $leerlingen->getId(),
-            $leerlingen->getVoornaam(),
-            $leerlingen->getAchternaam()
+            $leerlingen->getVoornaam()
         ));
     }
 }
+
+
